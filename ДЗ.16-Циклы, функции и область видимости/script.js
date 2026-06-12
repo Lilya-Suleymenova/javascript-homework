328bff9721f732f798954ac8860dc6bfc82e1511
Lilya-Suleymenova <suleymenova.lilya@mail.ru>
2026-06-12 12:48:47 +0500

    Update script.js

--- ДЗ.16-Циклы, функции и область видимости/script.js
+++ ДЗ.16-Циклы, функции и область видимости/script.js
@@ -1,1 +1,50 @@
 
+
+// Задание 1
+// Калькулятор вклада
+
+function calculateDeposit(amount, percent, years) {
+
+    for (let i = 1; i <= years; i++) {
+        amount = amount + (amount * percent / 100);
+    }
+
+    return amount;
+}
+
+function showDeposit() {
+
+    let result = calculateDeposit(200000, 10, 5);
+
+    console.log(`Через 5 лет на счете будет ${result.toFixed(2)} тенге`);
+
+    alert(`Через 5 лет на счете будет ${result.toFixed(2)} тенге`);
+}
+
+
+// Задание 2
+// Угадай число
+
+function guessGame() {
+
+    let secretNumber = 7;
+
+    while (true) {
+
+        let userInput = prompt(
+            'Угадай число от 1 до 10. Для выхода напиши "выход"'
+        );
+
+        if (userInput === null || userInput.toLowerCase() === 'выход') {
+            alert('Игра окончена');
+            break;
+        }
+
+        if (Number(userInput) === secretNumber) {
+            alert('Поздравляю! Вы угадали!');
+            break;
+        } else {
+            alert('Не угадал, попробуй еще раз!');
+        }
+    }
+}
