e857060525e9ce5268e0033a0b47dd3aca40813e
Lilya-Suleymenova <suleymenova.lilya@mail.ru>
2026-06-12 16:19:02 +0500

    Update script.js

--- ДЗ.8-DOM: доступ к элементам страницы/script.js
+++ ДЗ.8-DOM: доступ к элементам страницы/script.js
@@ -1,1 +1,36 @@
+// Задание 1
+// Инвентаризация
 
+const shoppingList = document.getElementById('shopping-list');
+
+const items = document.querySelectorAll('.item');
+
+console.log(
+    `Количество продуктов в списке: ${items.length}`
+);
+
+
+// Задание 2
+// Найти шпиона
+
+const secretPerson = document.querySelector('.secret');
+
+console.log(
+    `Найден: ${secretPerson.textContent}`
+);
+
+// Задание 3
+// Олдскул против Модерна
+
+// 1 вариант
+const btn1 = document.getElementById('submit-btn');
+
+// 2 вариант
+const btn2 = document.querySelector('#submit-btn');
+
+// 3 вариант
+const btn3 = document.querySelector('.primary');
+
+console.log(btn1);
+console.log(btn2);
+console.log(btn3);
