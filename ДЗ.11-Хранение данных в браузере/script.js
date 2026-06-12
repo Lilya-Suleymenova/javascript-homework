0d1bbb6d1fe9874887e3833b8454414617ce4357
Lilya-Suleymenova <suleymenova.lilya@mail.ru>
2026-06-12 17:59:41 +0500

    Update script.js

--- ДЗ.11-Хранение данных в браузере/script.js
+++ ДЗ.11-Хранение данных в браузере/script.js
@@ -1,1 +1,62 @@
+// Задание 1
+// Счетчик посещений
 
+let visits = localStorage.getItem('visits');
+
+if (visits === null) {
+    visits = 0;
+}
+
+visits = Number(visits);
+
+visits++;
+
+localStorage.setItem('visits', visits);
+
+console.log(`Вы заходили сюда уже ${visits} раз`);
+
+
+// Задание 2
+// Список задач
+
+function addTask(text) {
+
+    let tasks = localStorage.getItem('tasks');
+
+    if (tasks === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(tasks);
+    }
+
+    tasks.push(text);
+
+    localStorage.setItem(
+        'tasks',
+        JSON.stringify(tasks)
+    );
+}
+
+
+function showTasks() {
+
+    let tasks = localStorage.getItem('tasks');
+
+    if (tasks === null) {
+        console.log('Список задач пуст');
+        return;
+    }
+
+    tasks = JSON.parse(tasks);
+
+    for (let task of tasks) {
+        console.log(task);
+    }
+}
+
+
+addTask('Изучить localStorage');
+addTask('Сделать домашнее задание');
+addTask('Отправить работу преподавателю');
+
+showTasks();
